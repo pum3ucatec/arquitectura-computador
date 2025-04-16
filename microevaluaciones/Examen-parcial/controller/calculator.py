dbd19ec3171c7d4bbe9d183c07d542fff2a360be
fernadosaaavedra12 <saavedravargas624@gmail.com>
2025-04-15 20:30:00 -0400

    agregandoep-fernandosaavedra

--- microevaluaciones/Examen-parcial/controller/calculator.py
+++
@@ -1,6 +0,0 @@
-def sumar_binarios(bin1, bin2):
-    try:
-        suma = bin(int(bin1, 2) + int(bin2, 2))[2:]
-        return suma
-    except ValueError:
-        return "Error"
--- microevaluaciones/Examen-parcial/ep-FernandoSaavedra/controller/utils/validator.py
+++
@@ -1,2 +0,0 @@
-def es_binario(valor):
-    return all(c in '01' for c in valor)
